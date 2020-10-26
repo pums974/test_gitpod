967953c4dcdf43446b130aae9275576e00cd4e65
pums974 <pums974@gmail.com>
2020-10-26 17:00:43 +0000

    modif source

--- main.f90
+++ main.f90
@@ -1,12 +1,12 @@
 program test
-use toto_mod, only: toto
-complex, allocatable :: truc(:, :)
-integer :: i, j
-allocate(truc(-5:5, 7:10))
-do i = -5,5
-do j = 7,10
-    truc(i, j) = complex(i,j)
-end do
-end do
-call toto(truc)
+    use toto_mod, only: toto
+    complex, allocatable :: truc(:, :)
+    integer :: i, j    
+    allocate(truc(-5:5, 7:10))
+    do i = -5,5
+        do j = 7,10
+            truc(i, j) = complex(i,j)
+        end do
+    end do
+    call toto(truc)
 end program test
--- toto_mod.f90
+++ toto_mod.f90
@@ -1,9 +1,16 @@
 module toto_mod
+implicit none
 
 contains
 
     subroutine toto(test)
+        implicit none
         complex, allocatable, intent(in) :: test(:, :)
+        integer :: i
+        double precision :: u
+
+        i = i+1
+        u = 3.*i 
 
         print*, lbound(test)
         print*, ubound(test)
