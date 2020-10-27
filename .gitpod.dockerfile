c9529cb382f2181f4294a247ccc8add09442f2b4
pums974 <pums974@gmail.com>
2020-10-27 10:40:49 +0000

    update gitpod config

--- .gitpod.dockerfile
+++ .gitpod.dockerfile
@@ -2,5 +2,5 @@ FROM gitpod/workspace-full
 
 RUN sudo apt-get update \
  && sudo apt-get install -y \
-    gfortran fortran-language-server\
+    gfortran fortran-language-server valgrind\
  && sudo rm -rf /var/lib/apt/lists/*
--- main.f90
+++ main.f90
@@ -1,7 +1,12 @@
 program test
     use toto_mod, only: toto
     complex, allocatable :: truc(:, :)
-    integer :: i, j    
+    integer :: i, j
+    double precision :: u
+
+    i = i+1
+    u = 3.*i 
+
     allocate(truc(-5:5, 7:10))
     do i = -5,5
         do j = 7,10
